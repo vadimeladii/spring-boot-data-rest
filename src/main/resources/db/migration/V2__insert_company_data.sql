03d81e027cb96f4122806ddbe43ad2497ce93148
Vadim Eladii <veladii@straighterline.com>
2019-12-25 23:00:08 +0200

    Create migration script for insert company data

---
+++ src/main/resources/db/migration/V2__insert_company_data.sql
@@ -0,0 +1,3 @@
+INSERT INTO company(id, name, activity) VALUES(1, 'Company1', 'Activity1');
+INSERT INTO company(id, name, activity) VALUES(2, 'Company2', 'Activity2');
+INSERT INTO company(id, name, activity) VALUES(3, 'Company3', 'Activity3');
