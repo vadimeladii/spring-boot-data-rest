f9225609c19ad7fd8ff530b0e758dfe4d2ad0a30
vadimeladii <veladii@straighterline.com>
2019-12-27 03:28:31 +0200

    Change version of Spring parent to be compatible with springfox and add springfox-swagger2, springfox-data-rest and springfox-swagger-ui

--- src/main/java/md/springboot/SpringDataRestApplication.java
+++ src/main/java/md/springboot/SpringDataRestApplication.java
@@ -1,8 +1,13 @@
 package md.springboot;
 
+import md.springboot.config.SpringFoxConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
+@Import(SpringFoxConfiguration.class)
 @SpringBootApplication
 public class SpringDataRestApplication {
 
